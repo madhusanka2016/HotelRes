bd0929a32331387bdb0454a380aa669706a9e14f
madhusanka2016 <smmailofficial@gmail.com>
2019-04-07 08:51:59 +0530

    customer changes Ok

--- admin/customerdetails.php
+++ admin/customerdetails.php
@@ -3,6 +3,32 @@ session_start();
 if (!isset($_SESSION["user"])) {
     header("location:index.php");
 }
+if(isset($_POST['update'])){
+    include ('db.php');
+    
+
+    $id = $_POST['id'];
+    $title = $_POST['title']; 
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $nation = $_POST['nation'];
+    $country = $_POST['country'];
+    $phone = $_POST['phone'];
+				
+    $newsql ="update `roombook` set `Title`='$title',`FName`='$fname',`LName`='$lname',`Email`='$email',`National`='$nation',`Country`='$country',`Phone`='$phone' WHERE id ='$id' ";
+    if(mysqli_query($con,$newsql))
+			 	{
+                    echo' <script language="javascript" type="text/javascript"> alert("Customer Updated") </script>';
+
+                    echo "<script type='text/javascript'> window.location='customerdetails.php'</script>";
+                    
+						
+			 	}
+
+}
+
+
 
 
 include('db.php');
@@ -154,12 +180,14 @@ ob_start();
                                         <h4 class="modal-title" id="myModalLabel">Edit Customer Details - '. $row['id'] .'</h4>
                                     </div>
                                     <form method="post">
+                                    <input type="hidden" name="id" value="'. $row['id'] .'" />
+
                                             
                                         <div class="modal-body">
                                                 <div class="form-group">
                                                 <label>Title*</label>
-                                                <select name="title" class="form-control" required >
-                                                    <option value selected ></option>
+                                                <select name="title" class="form-control"  >
+                                                    <option value selected value="'. $row['Title'] .'">'. $row['Title'] .'</option>
                                                     <option value="Dr.">Dr.</option>
                                                     <option value="Miss.">Miss.</option>
                                                     <option value="Mr.">Mr.</option>
@@ -171,17 +199,17 @@ ob_start();
                                                 </div>
                                                 <div class="form-group">
                                             <label>First Name</label>
-                                            <input name="fname" class="form-control" required>
+                                            <input name="fname" class="form-control" required value='. $row['FName'] .'>
                                             
                                             </div>
                                             <div class="form-group">
                                                             <label>Last Name</label>
-                                                            <input name="lname" class="form-control" required>
+                                                            <input name="lname" class="form-control" value='. $row['LName'] .' required>
                                                             
                                             </div>
                                             <div class="form-group">
                                                             <label>Email</label>
-                                                            <input name="email" type="email" class="form-control" required>
+                                                            <input name="email" type="email" class="form-control" value='. $row['Email'].' required>
                                                             
                                             </div>
                                             <div class="form-group">
@@ -194,12 +222,31 @@ ob_start();
                                                             </label>
                                         
                                                 </div>
-                                            
+                                                
+                                                <div class="form-group">
+                                                            <label>Phone</label>
+                                                            <input name="phone" class="form-control" value='. $row['Phone'] .' required>
+                                                            
+                                            </div>';
 
+                                        $countries = array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
+                                            
+                                        echo'
+                                        <div class="form-group">
+                                            <label>Passport Country*</label>
+                                            <select name="country" class="form-control" >
+												<option value selected value="'. $row['Country'] .'" >'. $row['Country'] .'</option>';
+                                                
+												foreach($countries as $key => $value):
+												echo '<option value="'.$value.'">'.$value.'</option>'; //close your tags!!
+												endforeach;
+											echo'
+                                            </select>
+								</div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 
-                                            <input type="submit" name="in" value="Add" class="btn btn-primary">
+                                            <input type="submit" name="update" value="Update" class="btn btn-primary">
                                                 </form>
 
                                         </div>
