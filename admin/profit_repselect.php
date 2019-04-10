eaff588684d5166357faaf92268b26025304335b
unknown <smmailofficial@gmail.com>
2019-04-10 10:44:44 +0530

    profit report generation

---
+++ admin/profit_repselect.php
@@ -0,0 +1,293 @@
+<?php  
+session_start();  
+if(!isset($_SESSION["user"]))
+{
+ header("location:index.php");
+}
+include('db.php');
+
+$user_id = $_SESSION['user_id'];
+$sql2 = "SELECT * FROM login WHERE id = '$user_id'";
+$result2 = mysqli_query($con, $sql2);
+$userRow = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+
+if ($userRow['role'] != "admin" || $userRow['role'] != "admin") {
+    header("location:home.php");
+}
+$Page_title = 'HORTAINRISE HOTEL - Salary';
+
+
+
+
+
+?> 
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $Page_title ?></title>
+	<!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php">MAIN MENU </a>
+            </div>
+
+            <ul class="nav navbar-top-links navbar-right">
+			
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="usersetting.php"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="settings.php"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+					
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+        </nav>
+        <!--/. NAV TOP  -->
+        <?php include('includes/sidebar.php'); ?>
+
+        <!-- /. NAV SIDE  -->
+       
+        
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                           Profit Report <small></small>
+                        </h1>
+                    </div>
+                </div> 
+                 
+                                 
+            <div class="row">
+                
+                <div class="col-md-5 col-sm-5">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            Employee Salary
+                        </div>
+                        <div class="panel-body">
+						<form name="form" method="post" action="showsalary.php">
+                            <div class="form-group">
+                                            <label>Employee</label>
+                                            <select name="emp"  class="form-control" required>
+                                                
+                                                
+                        <?php
+						$sqle = "select * from employee where status = 1";
+						$ree = mysqli_query($con,$sqle);
+                                                
+                                                
+                                                while($row= mysqli_fetch_array($ree))
+										{
+                                                    echo "<option value = '".$row['id']."'>".$row['name']."</option>";
+                                                    
+                                                    
+                                                }
+                                                
+                                                
+						?>
+                                                
+                                            </select>
+                              </div>
+							  
+								<div class="form-group">
+                                            <label>Year</label>
+                                            <select name="year" class="form-control" required>
+                                                        <option value="2019">2019 </option>
+                                                        <option value="2020">2020 </option>
+                                                        <option value="2021">2021 </option>
+                                                        <option value="2022">2022 </option>
+                                                        <option value="2023">2023 </option>
+                                                        <option value="2024">2024 </option>
+                                                        <option value="2025">2025 </option>
+                                                        <option value="2026">2026 </option>
+                                                        <option value="2027">2027 </option>
+                                                        <option value="2028">2028 </option>
+                                                        <option value="2029">2029 </option>
+                                                        <option value="2030">2030 </option>                         
+                                            </select>
+                                            
+                               </div>
+                               <div class="form-group">
+                                            <label>Month</label>
+                                            <select name="month" class="form-control" required>
+                                            
+                                                        <option value="01" >January </option>
+                                                        <option value="02" >February </option>
+                                                        <option value="03" >March </option>
+                                                        <option value="04" >April </option>
+                                                        <option value="05" >May</option>
+                                                        <option value="06" >June </option>
+                                                        <option value="07" >July </option>
+                                                        <option value="08" >August </option>
+                                                        <option value="09" >September </option>
+                                                        <option value="10" >October </option>
+                                                        <option value="11" >November </option>
+                                                        <option value="12" >December </option>                         
+                                            </select>
+                                            
+                               </div>
+							 <input type="submit" name="print" value="Print" class="btn btn-primary"> 
+							</form>
+							<?php
+							 include('db.php');
+							 if(isset($_POST['add']))
+							 {
+										$empid = $_POST['emp'];
+									
+										$status = $_POST['status'];
+									
+										
+										$check="SELECT * FROM attendance WHERE date = curdate() and emp_id = '$empid' and status =   '$status'";
+										$rs = mysqli_query($con,$check);
+										$data = mysqli_fetch_array($rs, MYSQLI_NUM);
+										if($data[0] > 1) {
+											echo "<script type='text/javascript'> alert('Employee Already marked')</script>";
+											
+										}
+
+										else
+										{
+							 
+										
+										$sql ="INSERT INTO `attendance`( date, employee, emp_id, status, time) VALUES (curdate(),(select name from employee where id = '$empid'),'$empid', '$status', now())" ;
+										if(mysqli_query($con,$sql))
+										{
+										 echo '<script>alert("Marked") </script>' ;
+										}else {
+											echo '<script>alert("Sorry ! Check The System") </script>' ;
+										}
+							 }
+							}
+
+							?>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                  
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            Recent Attendance
+                        </div>
+                        <div class="panel-body">
+								<!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <?php
+						$sql = "select * from attendance where date = curdate() order by emp_id";
+						$re = mysqli_query($con,$sql)
+						?>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Employee</th>
+                                            <th>Time</th>
+                                            <th>Status</th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+									
+									<?php
+										while($row= mysqli_fetch_array($re))
+										{
+												$id = $row['id'];
+											if($id % 2 == 0) 
+											{
+												echo "<tr class=odd gradeX>
+													<td>".$row['employee']."</td>
+                                                                                                            <td>".$row['time']."</td>
+													<td>".$row['status']."</td>
+												</tr>";
+											}
+											else
+											{
+												echo"<tr class=even gradeC>
+													<td>".$row['employee']."</td>
+                                                                                                            <td>".$row['time']."</td>
+													<td>".$row['status']."</td>
+												</tr>";
+											
+											}
+										}
+									?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!--End Advanced Tables -->
+                    
+                       
+                            
+							  
+							 
+							 
+							  
+							  
+							   
+                       </div>
+                        
+                    </div>
+                </div>
+                
+               
+            </div>
+                    
+            
+				
+					</div>
+			 <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+    
+   
+</body>
+</html>
